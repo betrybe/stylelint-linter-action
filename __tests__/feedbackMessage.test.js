b38918eff04e46df0e8a544de535facec0ca717a
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-11-10 16:48:21 -0300

    impl: ajusta todos os testes

--- __tests__/feedbackMessage.test.js
+++ __tests__/feedbackMessage.test.js
@@ -1,13 +1,13 @@
 const buildFeedbackMessage = require('../feedbackMessage');
-const multipleErrorsOneFile = require('./fixtures/eslint-results/multipleErrorsOneFile.json');
-const multipleWarningsOneFile = require('./fixtures/eslint-results/multipleWarningsOneFile.json');
-const multipleWarningsAndErrorsOneFile = require('./fixtures/eslint-results/multipleWarningsAndErrorsOneFile.json');
-const multipleErrorsMultipleFiles = require('./fixtures/eslint-results/multipleErrorsMultipleFiles.json');
-const multipleWarningsMultipleFiles = require('./fixtures/eslint-results/multipleWarningsMultipleFiles.json');
-const noError = require('./fixtures/eslint-results/frontEndNoError.json');
-const oneError = require('./fixtures/eslint-results/oneError.json');
-const oneErrorOneFileMultipleWarningsAnotherFile = require('./fixtures/eslint-results/oneErrorOneFileMultipleWarningsAnotherFile.json');
-const oneWarning = require('./fixtures/eslint-results/oneWarning.json');
+const multipleErrorsOneFile = require('./fixtures/stylelint-results/multipleErrorsOneFile.json');
+const multipleWarningsOneFile = require('./fixtures/stylelint-results/multipleWarningsOneFile.json');
+const multipleWarningsAndErrorsOneFile = require('./fixtures/stylelint-results/multipleErrorsAndWarningsOneFile.json');
+const multipleErrorsMultipleFiles = require('./fixtures/stylelint-results/multipleErrorsMultipleFiles.json');
+const multipleWarningsMultipleFiles = require('./fixtures/stylelint-results/multipleWarningsMultipleFiles.json');
+const noError = require('./fixtures/stylelint-results/noError.json');
+const oneError = require('./fixtures/stylelint-results/oneError.json');
+const oneErrorOneFileMultipleWarningsAnotherFile = require('./fixtures/stylelint-results/oneErrorOneFileMultipleWarningsAnotherFile.json');
+const oneWarning = require('./fixtures/stylelint-results/oneWarning.json');
 
 describe('Feedback message', () => {
   describe('No issue is found', () => {
@@ -30,9 +30,9 @@ describe('Feedback message', () => {
     expect(buildFeedbackMessage(oneError, './')).toBe(
       '### Foi encontrado 1 erro.\n' +
       '\n' +
-      '#### Arquivo `/my-project/index.js`\n' +
+      '#### Arquivo `/frontend/main.css`\n' +
       '\n' +
-      '- Linha **1**: Function \'isPentagon\' has too many parameters (5). Maximum allowed is 4.\n' +
+      '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
       '\n' +
       '### Nenhum aviso encontrado.'
     );
@@ -41,14 +41,12 @@ describe('Feedback message', () => {
   describe('Multiple errors are found', () => {
     test('When all errors are contained in one file, a message listing all those errors is returned', () => {
       expect(buildFeedbackMessage(multipleErrorsOneFile, './')).toBe(
-        '### Foram encontrados 4 erros.\n' +
+        '### Foram encontrados 2 erros.\n' +
         '\n' +
-        '#### Arquivo `/my-react-project/src/components/Greeting.js`\n' +
+        '#### Arquivo `/frontend/main.css`\n' +
         '\n' +
-        '- Linha **3**: \'name\' is missing in props validation\n' +
-        '- Linha **3**: `Hello, ` must be placed on a new line\n' +
-        '- Linha **3**: `{name}` must be placed on a new line\n' +
-        '- Linha **5**: Missing semicolon.\n' +
+        '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
         '\n' +
         '### Nenhum aviso encontrado.'
       );
@@ -56,17 +54,17 @@ describe('Feedback message', () => {
 
     test('When the errors span multiple files, a message listing all those errors is returned', () => {
       expect(buildFeedbackMessage(multipleErrorsMultipleFiles, './')).toBe(
-        '### Foram encontrados 4 erros.\n' +
+        '### Foram encontrados 5 erros.\n' +
         '\n' +
-        '#### Arquivo `/my-react-project/src/App.js`\n' +
+        '#### Arquivo `/frontend/main.css`\n' +
         '\n' +
-        '- Linha **2**: `react` import should occur before import of `./components/Greeting`\n' +
-        '- Linha **12**: `code` must be placed on a new line\n' +
-        '- Linha **24**: Expected indentation of 6 space characters but found 4.\n' +
-        '' +
-        '#### Arquivo `/my-react-project/src/components/Greeting.js`\n' +
+        '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
+        '- Linha **9**: Unexpected unknown unit \"pixels\" (unit-no-unknown)\n' +
+        '#### Arquivo `/frontend/sidebar.css`\n' +
         '\n' +
-        '- Linha **3**: \'name\' is missing in props validation\n' +
+        '- Linha **8**: Unexpected duplicate name serif (font-family-no-duplicate-names)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
         '\n' +
         '### Nenhum aviso encontrado.'
       );
@@ -78,9 +76,9 @@ describe('Feedback message', () => {
       '### Nenhum erro encontrado.\n' +
       '### Foi encontrado 1 aviso.\n' +
       '\n' +
-      '#### Arquivo `/back-end/index.js`\n' +
+      '#### Arquivo `/frontend/main.css`\n' +
       '\n' +
-      '- Linha **8**: Unexpected console statement.\n'
+      '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)'
     );
   });
 
@@ -90,26 +88,28 @@ describe('Feedback message', () => {
         '### Nenhum erro encontrado.\n' +
         '### Foram encontrados 2 avisos.\n' +
         '\n' +
-        '#### Arquivo `/front-end/src/App.js`\n' +
+        '#### Arquivo `/frontend/main.css`\n' +
         '\n' +
-        '- Linha **7**: Unexpected console statement.\n' +
-        '- Linha **28**: Unexpected alert.\n'
+        '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)'
       );
     });
 
     test('When the warnings span multiple files, a message listing all those warnings is returned', () => {
       expect(buildFeedbackMessage(multipleWarningsMultipleFiles, './')).toBe(
        '### Nenhum erro encontrado.\n' +
-       '### Foram encontrados 3 avisos.\n' +
+       '### Foram encontrados 5 avisos.\n' +
        '\n' +
-       '#### Arquivo `/front-end/src/App.js`\n' +
+       '#### Arquivo `/frontend/main.css`\n' +
        '\n' +
-       '- Linha **7**: Unexpected console statement.\n' +
-       '- Linha **28**: Unexpected alert.\n' +
+       '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+       '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
+       '- Linha **9**: Unexpected unknown unit \"pixels\" (unit-no-unknown)\n' +
        '' +
-       '#### Arquivo `/front-end/src/components/Greeting.js`\n' +
+       '#### Arquivo `/frontend/sidebar.css`\n' +
        '\n' +
-       '- Linha **7**: Missing trailing comma.\n'
+       '- Linha **8**: Unexpected duplicate name serif (font-family-no-duplicate-names)\n' +
+       '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)'
       );
     });
   });
@@ -117,18 +117,19 @@ describe('Feedback message', () => {
   describe('Errors and warnings are found', () => {
     test('When all errors and warnings are contained in one file, a message listing both errors and warnings is returned', () => {
       expect(buildFeedbackMessage(multipleWarningsAndErrorsOneFile, './')).toBe(
-        '### Foi encontrado 1 erro.\n' +
+        '### Foram encontrados 2 erros.\n' +
         '\n' +
-        '#### Arquivo `/front-end/src/App.js`\n'+
+        '#### Arquivo `/frontend/main.css`\n'+
         '\n' +
-        '- Linha **33**: Newline required at end of file but not found.\n' +
+        '- Linha **9**: Unexpected duplicate name serif (font-family-no-duplicate-names)\n' +
+        '- Linha **10**: Unexpected unknown unit \"pixels\" (unit-no-unknown)\n' +
         '\n' +
         '### Foram encontrados 2 avisos.\n' +
         '\n' +
-        '#### Arquivo `/front-end/src/App.js`\n'+
+        '#### Arquivo `/frontend/main.css`\n'+
         '\n' +
-        '- Linha **7**: Unexpected console statement.\n' +
-        '- Linha **28**: Unexpected alert.\n'
+        '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)'
       );
     });
 
@@ -136,33 +137,35 @@ describe('Feedback message', () => {
       expect(buildFeedbackMessage(oneErrorOneFileMultipleWarningsAnotherFile, './')).toBe(
         '### Foi encontrado 1 erro.\n' +
         '\n' +
-        '#### Arquivo `/front-end/src/components/Greeting.js`\n'+
+        '#### Arquivo `/frontend/main.css`\n'+
         '\n' +
-        '- Linha **2**: Missing semicolon.\n' +
+        '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
         '\n' +
-        '### Foram encontrados 2 avisos.\n' +
+        '### Foram encontrados 3 avisos.\n' +
         '\n' +
-        '#### Arquivo `/front-end/src/App.js`\n'+
+        '#### Arquivo `/frontend/sidebar.css`\n'+
         '\n' +
-        '- Linha **7**: Unexpected console statement.\n' +
-        '- Linha **28**: Unexpected alert.\n'
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
+        '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
+        '- Linha **9**: Unexpected unknown unit \"pixels\" (unit-no-unknown)'
       );
     });
   });
 
   test('The root directory path for the project isn\'t displayed for each file', () => {
     expect(buildFeedbackMessage(multipleErrorsMultipleFiles, '/my-react-project')).toBe(
-      '### Foram encontrados 4 erros.\n' +
+      '### Foram encontrados 5 erros.\n' +
       '\n' +
-      '#### Arquivo `/src/App.js`\n' +
+      '#### Arquivo `/frontend/main.css`\n' +
       '\n' +
-      '- Linha **2**: `react` import should occur before import of `./components/Greeting`\n' +
-      '- Linha **12**: `code` must be placed on a new line\n' +
-      '- Linha **24**: Expected indentation of 6 space characters but found 4.\n' +
+      '- Linha **5**: Unexpected invalid hex color \"#ye\" (color-no-invalid-hex)\n' +
+      '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
+      '- Linha **9**: Unexpected unknown unit \"pixels\" (unit-no-unknown)\n' +
       '' +
-      '#### Arquivo `/src/components/Greeting.js`\n' +
+      '#### Arquivo `/frontend/sidebar.css`\n' +
       '\n' +
-      '- Linha **3**: \'name\' is missing in props validation\n' +
+      '- Linha **8**: Unexpected duplicate name serif (font-family-no-duplicate-names)\n' +
+      '- Linha **4**: Unexpected unknown property \"nokey\" (property-no-unknown)\n' +
       '\n' +
       '### Nenhum aviso encontrado.'
     );
